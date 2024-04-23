667f1c1163ca7aa1bf5b57ea83485a70f6c21828
deoache <daniel.ocampoh@udea.edu.co>
2024-04-23 18:51:29 +0000

    add submitters

---
+++ submit.py
@@ -0,0 +1,137 @@
+import json
+import time
+import pickle
+import argparse
+from coffea import processor
+from analysis.processors.tag_eff import TaggingEfficiencyProcessor
+
+# from analysis.processors.signal import SignalProcessor
+
+from coffea.nanoevents import NanoEventsFactory, PFNanoAODSchema
+
+
+def main(args):
+    # define processors and executors
+    processors = {
+        "tag_eff": TaggingEfficiencyProcessor,
+    }
+    processor_args = {
+        "tag_eff": {
+            "year": args.year,
+            "tagger": args.tagger,
+            "flavor": args.flavor,
+            "wp": args.wp,
+        }
+    }
+    executors = {
+        "iterative": processor.iterative_executor,
+        "futures": processor.futures_executor,
+        "dask": processor.dask_executor,
+    }
+    executor_args = {
+        "schema": PFNanoAODSchema,
+    }
+    if args.executor == "futures":
+        executor_args.update({"workers": 4})
+        
+    
+    # run processor
+    with open(args.fileset) as f:
+        fileset = json.load(f)
+    t0 = time.monotonic()
+    out = processor.run_uproot_job(
+        fileset,
+        treename="Events",
+        processor_instance=processors[args.processor](**processor_args[args.processor]),
+        executor=executors[args.executor],
+        executor_args=executor_args,
+    )
+    exec_time = format_timespan(time.monotonic() - t0)
+
+    print(f"Execution time: {exec_time}")
+    with open(f"{args.output_path}/{args.sample}.pkl", "wb") as handle:
+        pickle.dump(out, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--processor",
+        dest="processor",
+        type=str,
+        default="tag_eff",
+        help="processor to be used {tag_eff}",
+    )
+    parser.add_argument(
+        "--executor",
+        dest="executor",
+        type=str,
+        default="iterative",
+        help="executor",
+    )
+    parser.add_argument(
+        "--sample",
+        dest="sample",
+        type=str,
+        default="",
+        help="sample to be processed",
+    )
+    parser.add_argument(
+        "--fileset",
+        dest="fileset",
+        type=str,
+        default="",
+        help="fileset path",
+    )
+    parser.add_argument(
+        "--year",
+        dest="year",
+        type=str,
+        default="",
+        help="year of the data {2022EE, 2022, 2023}",
+    )
+    parser.add_argument(
+        "--output_path",
+        dest="output_path",
+        type=str,
+        default="",
+        help="output path",
+    )
+    parser.add_argument(
+        "--tagger",
+        dest="tagger",
+        type=str,
+        default="pnet",
+        help="tagger {pnet, part, deepjet}",
+    )
+    parser.add_argument(
+        "--wp",
+        dest="wp",
+        type=str,
+        default="tight",
+        help="working point {loose, medium, tight}",
+    )
+    parser.add_argument(
+        "--flavor",
+        dest="flavor",
+        type=str,
+        default="c",
+        help="Hadron flavor {c, b}",
+    )
+    parser.add_argument(
+        "--workers",
+        dest="workers",
+        type=int,
+        default=4,
+        help="number of workers to use with futures executor (default 4)",
+    )
+    parser.add_argument(
+        "--nfiles",
+        dest="nfiles",
+        type=int,
+        default=-1,
+        help="number of .root files to be processed by sample. To run all files use -1 (default 1)",
+    )
+
+    args = parser.parse_args()
+    main(args)
---
+++ submit_condor.py
@@ -0,0 +1,86 @@
+import os
+import glob
+import argparse
+from pathlib import Path
+from condor.utils import submit_condor
+from analysis.configs.load_config import load_config
+from analysis.filesets.utils import build_filesets
+
+
+def main(args):
+    args = vars(args)
+    build_filesets(args)
+    filesets_path = f"{Path.cwd()}/analysis/filesets/{args['year']}"
+    filesets = glob.glob(f"{filesets_path}/{args['sample']}*.json")
+
+    dataset_config = load_config(
+        config_type="dataset", config_name=args["sample"], year=args["year"]
+    )    
+    
+    output_path = Path(Path.cwd() / "outputs" / args["processor"] / args["year"])
+    if not output_path.exists():
+        output_path.mkdir(parents=True)
+    args["output_path"] = str(output_path)
+
+    for partition, fileset in enumerate(filesets, start=1):
+        args["fileset"] = fileset
+        args["cmd"] = (
+            "python3 submit.py "
+            f"--processor {args['processor']} "
+            f"--sample {args['sample']} "
+            f"--year {args['year']} "
+            f"--output_path {args['output_path']} "
+            f"--workers {args['workers']} "
+            f"--nfiles {args['nfiles']} "
+            f"--executor {args['executor']} "
+            f"--fileset {args['fileset']} "
+        )
+        submit_condor(args)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--processor",
+        dest="processor",
+        type=str,
+        default="tag_eff",
+        help="processor to be used {tag_eff, signal}",
+    )
+    parser.add_argument(
+        "--sample",
+        dest="sample",
+        type=str,
+        default="",
+        help="sample to be processed",
+    )
+    parser.add_argument(
+        "--year",
+        dest="year",
+        type=str,
+        default="2022EE",
+        help="year of the data {2022EE, 2022, 2023}",
+    )
+    parser.add_argument(
+        "--executor",
+        dest="executor",
+        type=str,
+        default="futures",
+        help="executor to be used {iterative, futures, dask} (default iterative)",
+    )
+    parser.add_argument(
+        "--workers",
+        dest="workers",
+        type=int,
+        default=4,
+        help="number of workers to use with futures executor (default 4)",
+    )
+    parser.add_argument(
+        "--nfiles",
+        dest="nfiles",
+        type=int,
+        default=-1,
+        help="number of .root files to be processed by sample. To run all files use -1 (default 1)",
+    )
+    args = parser.parse_args()
+    main(args)
